3abb36ba3276ec45f10d3a0087894e7da8abe97c
Surasith Kaewvikkit <surasith.kae@ascendcorp.com>
2022-04-28 11:37:42 +0700

    update change port and response type

--- src/main/java/api/basic/surasith/com/controller/HealthController.java
+++ src/main/java/api/basic/surasith/com/controller/HealthController.java
@@ -5,21 +5,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import api.basic.surasith.com.entity.ApiResponseEntity;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/Health")
 public class HealthController {
 
     @GetMapping(path = "check")
-    public ResponseEntity<ApiResponseEntity> getHealth(){
-        ApiResponseEntity status = new ApiResponseEntity(HttpStatus.OK.value(), "Success");
+    public ResponseEntity getHealth(){
 
-        try{
-            return new ResponseEntity<ApiResponseEntity>(status, HttpStatus.OK);
-        }catch (Exception ex){
-            System.out.println(ex.getMessage());
-            throw ex;
-        }
+        HashMap<String, String> response = new HashMap();
+        response.put("status", HttpStatus.OK.name());
+        response.put("message", "Success");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
